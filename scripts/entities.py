019c16100cc50fe82c80873e0bf125df7ef1d130
Bruno Cabral <cabral.brc@gmail.com>
2023-10-11 12:28:11 -0700

    collision with tiles

--- scripts/entities.py
+++ scripts/entities.py
@@ -4,6 +4,7 @@ import pygame
 
 if TYPE_CHECKING:
     from game import Game
+    from scripts.tilemap import Tilemap
 
 
 class PhysicsEntity:
@@ -14,15 +15,39 @@ class PhysicsEntity:
         self.size = list(size)  # make a copy, if not, will be reference
         self.velocity = [0.0, 0.0]
 
-    def update(self, movement=(0, 0)):
+    def update(self, tilemap: Tilemap, movement=(0, 0)):
         frame_movement = (
             movement[0] + self.velocity[0],
             movement[1] + self.velocity[1],
         )
         self.pos[0] += frame_movement[0]
+        # collision on X axis
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                # pygame Rect() only deals with Integers
+                self.pos[0] = entity_rect.x
+
         self.pos[1] += frame_movement[1]
+        # collision on Y axis
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                # pygame Rect() only deals with Integers
+                self.pos[1] = entity_rect.y
 
         self.velocity[1] = min(5.0, self.velocity[1] + 0.1)
 
     def render(self, surf: pygame.Surface):
         surf.blit(self.game.assets["player"], self.pos)
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -5,6 +5,19 @@ import pygame
 if TYPE_CHECKING:
     from game import Game
 
+NEIGHBOR_OFFSETS = [
+    (-1, 0),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (0, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+]
+PHYSICS_TILES = {"grass", "stone"}
+
 
 class Tilemap:
     def __init__(self, game: Game, tile_size=16):
@@ -35,3 +48,28 @@ class Tilemap:
                 self.game.assets[tile["type"]][tile["variant"]],
                 (tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size),
             )
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = int(pos[0] // self.tile_size), int(pos[1] // self.tile_size)
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = (
+                str(tile_loc[0] + offset[0]) + ";" + str(tile_loc[1] + offset[1])
+            )
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile["type"] in PHYSICS_TILES:
+                rects.append(
+                    pygame.Rect(
+                        tile["pos"][0] * self.tile_size,
+                        tile["pos"][1] * self.tile_size,
+                        self.tile_size,
+                        self.tile_size,
+                    )
+                )
+        return rects
