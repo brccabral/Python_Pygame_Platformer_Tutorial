85b51a585905af51546c5ca91264ed550df4100d
Bruno Cabral <cabral.brc@gmail.com>
2023-10-10 20:51:42 -0700

    game class

--- game.py
+++ game.py
@@ -1,16 +1,24 @@
 import sys
 import pygame
 
-pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Ninja Game")
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
-    clock.tick(60)
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
