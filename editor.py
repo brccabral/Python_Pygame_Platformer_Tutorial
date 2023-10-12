74c19affcd22b6c726070f74c375aff21c41b15f
Bruno Cabral <cabral.brc@gmail.com>
2023-10-11 20:26:53 -0700

    show current selected tile

--- editor.py
+++ editor.py
@@ -27,6 +27,10 @@ class Editor:
             "stone": load_images("tiles/stone"),
         }
 
+        self.tile_list = list(self.assets)
+        self.tile_group = 0
+        self.tile_variant = 0
+
         # camera
         self.scroll = [0.0, 0.0]
 
@@ -34,6 +38,13 @@ class Editor:
         while True:
             # clear screen
             self.display.fill((0, 0, 0, 0))
+
+            current_tile_img: pygame.Surface = self.assets[
+                self.tile_list[self.tile_group]
+            ][self.tile_variant].copy()
+            current_tile_img.set_alpha(100)
+
+            self.display.blit(current_tile_img, (5, 5))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
